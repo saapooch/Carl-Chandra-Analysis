d95838a1d565aa53fa933337ba42709ddcf5a01b
Saahith Pochiraju <saahith116@gmail.com>
2017-10-22 23:19:43 -0400

    added ema and risk analysis to analysis folder

--- analysis/getRisk.py
+++ analysis/getRisk.py
@@ -1,5 +1,5 @@
 import quandl
-import pandas as pd 
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -31,7 +31,7 @@ for stock in stocks:
 	#Get data from Quandl database
 	data = quandl.get(databaseName, start_date=startDate, end_date=endDate)
 
-	#Quandl doesnt have a lot of holes, but this will fill in any NAN entries 
+	#Quandl doesnt have a lot of holes, but this will fill in any NAN entries
 	data = data.fillna(method = 'pad')
 
 	#make adj_close
@@ -47,27 +47,27 @@ print(portfolioDataFrame.head())
 
 #returns
 returns =  portfolioDataFrame.pct_change(1)
- 
+print(returns)
 mean_daily_returns = returns.mean()
 cov_matrix = returns.cov()
 
 #set number of runs of random portfolio weights
 num_portfolios = 25000
- 
+
 #set up array to hold results
 #We have increased the size of the array to hold the weight values for each stock
 results = np.zeros((numStocks+3,num_portfolios))
- 
+
 for i in range(num_portfolios):
     #select random weights for portfolio holdings
     weights = np.array(np.random.random(numStocks))
     #rebalance weights to sum to 1
     weights /= np.sum(weights)
- 
+	
     #calculate portfolio return and volatility
     portfolio_return = np.sum(mean_daily_returns * weights) * 252
     portfolio_std_dev = np.sqrt(np.dot(weights.T,np.dot(cov_matrix, weights))) * np.sqrt(252)
- 
+
     #store results in results array
     results[0,i] = portfolio_return
     results[1,i] = portfolio_std_dev
@@ -76,7 +76,7 @@ for i in range(num_portfolios):
     #iterate through the weight vector and add data to results array
     for j in range(len(weights)):
     	results[j+3,i] = weights[j]
- 
+
 #convert results array to Pandas DataFrame
 #results_frame = pd.DataFrame(results.T,columns=['ret','stdev','sharpe',stocks[0],stocks[1],stocks[2],stocks[3],stocks[4]])
 results_frame = pd.DataFrame(results.T)
@@ -90,7 +90,7 @@ results_frame.columns = nameArray
 max_sharpe_port = results_frame.iloc[results_frame['sharpe'].idxmax()]
 #locate positon of portfolio with minimum standard deviation
 min_vol_port = results_frame.iloc[results_frame['stdev'].idxmin()]
- 
+
 #create scatter plot coloured by Sharpe Ratio
 plt.scatter(results_frame.stdev,results_frame.ret,c=results_frame.sharpe,cmap='RdYlBu')
 plt.xlabel('Volatility')
